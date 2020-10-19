39f9ebe5391c9e90e415e8b768c3b854be97c004
Ivan Belikov <wanjas@gmail.com>
2020-10-19 14:44:44 +0900

    Script load fix

--- lib/useScript.tsx
+++ lib/useScript.tsx
@@ -2,27 +2,29 @@ import { useState, useEffect } from 'react';
 
 export function useScript(src: string) {
   // Keep track of script status ("idle", "loading", "ready", "error")
-  const [status, setStatus] = useState(src ? "loading" : "idle");
+  const [status, setStatus] = useState(src ? 'loading' : 'idle');
 
   useEffect(
     () => {
       // Allow falsy src value if waiting on other data needed for
       // constructing the script URL passed to this hook.
       if (!src) {
-        setStatus("idle");
+        setStatus('idle');
         return;
       }
 
       // Fetch existing script element by src
       // It may have been added by another intance of this hook
-      let script = document.querySelector(`script[src="${src}"]`) as HTMLScriptElement;
+      let script = document.querySelector(
+        `script[src="${src}"]`,
+      ) as HTMLScriptElement;
 
       if (!script) {
         // Create script
-        script = document.createElement("script");
+        script = document.createElement('script');
         script.src = src;
         script.async = true;
-        script.setAttribute("data-status", "loading");
+        script.setAttribute('data-status', 'loading');
         // Add script to document body
         document.body.appendChild(script);
 
@@ -30,38 +32,38 @@ export function useScript(src: string) {
         // This can be read by other instances of this hook
         const setAttributeFromEvent = (event: Event) => {
           script.setAttribute(
-            "data-status",
-            event.type === "load" ? "ready" : "error"
+            'data-status',
+            event.type === 'load' ? 'ready' : 'error',
           );
         };
 
-        script.addEventListener("load", setAttributeFromEvent);
-        script.addEventListener("error", setAttributeFromEvent);
+        script.addEventListener('load', setAttributeFromEvent);
+        script.addEventListener('error', setAttributeFromEvent);
       } else {
         // Grab existing script status from attribute and set to state.
-        setStatus(script.getAttribute("data-status") as string);
+        setStatus((script.getAttribute('data-status') as string) || 'ready');
       }
 
       // Script event handler to update status in state
       // Note: Even if the script already exists we still need to add
       // event handlers to update the state for *this* hook instance.
       const setStateFromEvent = (event: Event) => {
-        setStatus(event.type === "load" ? "ready" : "error");
+        setStatus(event.type === 'load' ? 'ready' : 'error');
       };
 
       // Add event listeners
-      script.addEventListener("load", setStateFromEvent);
-      script.addEventListener("error", setStateFromEvent);
+      script.addEventListener('load', setStateFromEvent);
+      script.addEventListener('error', setStateFromEvent);
 
       // Remove event listeners on cleanup
       return () => {
         if (script) {
-          script.removeEventListener("load", setStateFromEvent);
-          script.removeEventListener("error", setStateFromEvent);
+          script.removeEventListener('load', setStateFromEvent);
+          script.removeEventListener('error', setStateFromEvent);
         }
       };
     },
-    [src] // Only re-run effect if script src changes
+    [src], // Only re-run effect if script src changes
   );
 
   return status;
